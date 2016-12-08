9dff12435701427fae0fd99c1df4022843f80bd6
史小绮 <shixiaoqi@9hcom.com>
2016-12-08 11:21:05 +0800

    log add

--- src/main/java/com/view/controller/ViewController.java
+++ src/main/java/com/view/controller/ViewController.java
@@ -2,10 +2,13 @@ package com.view.controller;
 
 import com.view.service.ViewService;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Administrator on 2016/11/22.
@@ -14,9 +17,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class ViewController {
     @Autowired
     ViewService viewService;
+    Logger logger = LoggerFactory.getLogger("com.view.controller.ViewController");
     @RequestMapping(value = "test",method = RequestMethod.GET)
     public String test(){
         viewService.test();
         return "hello";
+    }
+    //日志
+    @RequestMapping(value = "logtest",method = RequestMethod.GET)
+    @ResponseBody
+    public String logtest(){
+        String name ="78787878";
+        logger.info("info ==>  name = {}",name);
+        logger.warn("warn ==>  name = {}",name);
+        logger.debug("debug ==>  name = {}",name);
+        logger.error("error ==>  name = {}",name);
+        return "yes";
     }
 }
